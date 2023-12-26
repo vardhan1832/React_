110ea868258719d89c83f1497609b0c18fa60268
vardhan1831 <sreevardhan259@gmail.com>
2023-12-26 13:51:51 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <div>
+             <h2>ExpenseItem</h2>
+            <ul>
+                <li>Food rs 10</li>
+                <li>Petrol rs 100</li>
+                <li>movies rs 200</li>
+            </ul>
+        </div> 
+    )
+}
+
+export default ExpenseItem
