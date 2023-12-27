8732be70c3be3cf61974c7316c5da6a20d038ecb
vardhan1831 <sreevardhan259@gmail.com>
2023-12-27 13:22:33 +0530

    added onClick event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,12 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const clickHandler = () =>{
+    console.log('Clicked!!')
+  }
+  const deleteHandler = () =>{
+
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,6 +16,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-location'> Location : {props.location}</div>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>change title</button>
+        <button onClick={deleteHandler}>delete</button>
       </div>
     </Card>
   );
