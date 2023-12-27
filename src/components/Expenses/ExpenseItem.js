c3d67cfabd45641368bdf0206c10deca288aacd4
vardhan1831 <sreevardhan259@gmail.com>
2023-12-27 14:12:46 +0530

    added useState hook for re-rendering

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,27 @@
+import React , {useState} from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title,setTitle] = useState(props.title)
   const clickHandler = () =>{
-    console.log('Clicked!!')
+    setTitle('Updated!!')
+    console.log(title)
   }
-  const deleteHandler = () =>{
-
+  const [amount,setAmount] = useState(props.amount)
+  const amountHandler = () =>{
+    setAmount(100)
   }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-location'> Location : {props.location}</div>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
         <button onClick={clickHandler}>change title</button>
-        <button onClick={deleteHandler}>delete</button>
+        <button onClick={amountHandler}>change amount</button>
       </div>
     </Card>
   );
